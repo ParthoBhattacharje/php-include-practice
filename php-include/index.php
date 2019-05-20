72150b32fa17d8958c175dc0420b7c8c3bac4536
Partho Bhattacharje <48360173+ParthoBhattacharje@users.noreply.github.com>
2019-05-20 17:11:10 +0600

    Update index.php

--- php-include/index.php
+++ php-include/index.php
@@ -1,16 +1,22 @@
-
+<!-- this is head-->
 <?php include_once('head.php'); ?>
 
+<!-- this is css-->
 <?php include_once('style.php'); ?>
 
+<!-- this is navbar-->
 <?php include_once('navbar.php'); ?>
 
+<!-- this is developer profile-->
 <?php include_once('developer.php'); ?>
 
+<!-- this is submission form-->
 <?php include_once('form.php'); ?>
 
+<!-- this is map-->
 <?php include_once('map.php'); ?>
 
+<!-- this is footer-->
 <?php include_once('footer&js.php') ?>
 
 
